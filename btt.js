e1d438c1c02f3b31cd4ad447610aac90316c6a64
nata7167 <natalie.vingaard09@gmail.com>
2025-06-02 15:49:27 +0200

    Update btt.js

--- btt.js
+++ btt.js
@@ -1,16 +1,20 @@
 const backToTopBtn = document.querySelector(".btt");
+let ticking = false;
 
-// Show/hide button on scroll
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
-    backToTopBtn.classList.add("visible");
-  } else {
-    backToTopBtn.classList.remove("visible");
-  }
-});
-
-
-// Scroll to top on click
-backToTopBtn.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
+window.addEventListener(
+  "scroll",
+  () => {
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        if (window.scrollY > 0) {
+          backToTopBtn.classList.add("visible");
+        } else {
+          backToTopBtn.classList.remove("visible");
+        }
+        ticking = false;
+      });
+      ticking = true;
+    }
+  },
+  { passive: true }
+);
