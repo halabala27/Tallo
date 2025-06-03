3c34d4413a90e4a4515bcbe21886053bdafdb52f
nata7167 <natalie.vingaard09@gmail.com>
2025-06-03 17:24:18 +0200

    loader:)

---
+++ loader.js
@@ -0,0 +1,8 @@
+
+window.addEventListener("load", function () {
+  const loader = document.getElementById("loader");
+  const content = document.querySelector(".content");
+
+  loader.style.display = "none";
+  content.style.display = "block";
+});
